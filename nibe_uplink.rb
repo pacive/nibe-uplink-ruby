97716506c4427205d93e1011ad0128f669697336
pacive <andersb86@gmail.com>
2018-11-25 17:36:45 +0100

    Add request method

--- nibe_uplink.rb
+++ nibe_uplink.rb
@@ -43,124 +43,108 @@ class NibeUplink
   # Get info on the current system
   def system
     return false unless @system_id
+
     uri = "#{API_ENDPOINT}/#{@system_id}"
-    response = @oauth.get(uri)
-    raise AuthorizationError, response.body if response.is_a? Net::HTTPUnauthorized
-    raise RateLimitError if response.is_a? Net::HTTPTooManyRequests 
-    raise ServerError if response.is_a? Net::HTTPServerError
-    response.body
+    request(:get, uri).body
   end
 
   # List all systems connected to the account
   def systems
-    response = @oauth.get(API_ENDPOINT)
-    raise AuthorizationError, response.body if response.is_a? Net::HTTPUnauthorized
-    raise RateLimitError if response.is_a? Net::HTTPTooManyRequests 
-    raise ServerError if response.is_a? Net::HTTPServerError
-    response.body
+    request(:get, API_ENDPOINT).body
   end
 
   # Get system status. Returns which subsystems are currently active
   def status
     return false unless @system_id
+
     uri = "#{API_ENDPOINT}/#{@system_id}/status/system"
-    response = @oauth.get(uri)
-    raise AuthorizationError, response.body if response.is_a? Net::HTTPUnauthorized
-    raise RateLimitError if response.is_a? Net::HTTPTooManyRequests 
-    raise ServerError if response.is_a? Net::HTTPServerError
-    response.body
+    request(:get, uri).body
   end
 
   # Get info on installed software and software updates
   def software
     return false unless @system_id
+
     uri = "#{API_ENDPOINT}/#{@system_id}/software"
-    response = @oauth.get(uri)
-    raise AuthorizationError, response.body if response.is_a? Net::HTTPUnauthorized
-    raise RateLimitError if response.is_a? Net::HTTPTooManyRequests 
-    raise ServerError if response.is_a? Net::HTTPServerError
-    response.body
+    request(:get, uri).body
   end
 
   # Get info and current values of the requested parameters,
   # or set parameters if a hash is provided as argument
   def parameters(parameters)
     return false unless @system_id
+
     uri = "#{API_ENDPOINT}/#{@system_id}/parameters"
-    response = nil
     if parameters.is_a?(Hash)
       body = { settings: parameters }
-      extheader = { 'content-type' => 'application/json' }
-      response = @oauth.put(uri, body.to_json, extheader)
+      request(:put, uri, nil, body.to_json).body
     else
-      response = @oauth.get(uri, 'parameterIds' => parameters)
+      request(:get, uri, 'parameterIds' => parameters).body
     end
-    raise AuthorizationError, response.body if response.is_a? Net::HTTPUnauthorized
-    raise RateLimitError if response.is_a? Net::HTTPTooManyRequests 
-    raise ServerError if response.is_a? Net::HTTPServerError
-    response.body
   end
 
   # Get notifications/alarms registered on the system
   def notifications
     return false unless @system_id
+
     uri = "#{API_ENDPOINT}/#{@system_id}/notifications"
-    response = @oauth.get(uri)
-    raise AuthorizationError, response.body if response.is_a? Net::HTTPUnauthorized
-    raise RateLimitError if response.is_a? Net::HTTPTooManyRequests 
-    raise ServerError if response.is_a? Net::HTTPServerError
-    response.body
+    request(:get, uri).body
   end
 
   # Get info on the system. If arg is set to true it returns parameters for all systems,
   # if set to a valid category, returns info on only that category
   def serviceinfo(arg = false)
     return false unless @system_id
+
     uri = "#{API_ENDPOINT}/#{@system_id}/serviceinfo/categories"
     uri += "/#{arg}" if arg.is_a? String
     query = { 'parameters' => true } if arg && !arg.is_a?(String)
-    response = @oauth.get(uri, query)
-    raise AuthorizationError, response.body if response.is_a? Net::HTTPUnauthorized
-    raise RateLimitError if response.is_a? Net::HTTPTooManyRequests 
-    raise ServerError if response.is_a? Net::HTTPServerError
-    response.body
+    request(:get, uri, query).body
   end
 
   # Get or set the smarthome mode
   def mode(mode = nil)
     return false unless @system_id
+
     uri = "#{API_ENDPOINT}/#{@system_id}/smarthome/mode"
-    response = nil
     if mode
       body = { mode: mode }
-      extheader = { 'content-type' => 'application/json' }
-      response = @oauth.put(uri, body.to_json, extheader)
+      request(:put, uri, nil, body.to_json).body
     else
-      response = @oauth.get(uri)
+      request(:get, uri).body
     end
-    raise AuthorizationError, response.body if response.is_a? Net::HTTPUnauthorized
-    raise RateLimitError if response.is_a? Net::HTTPTooManyRequests 
-    raise ServerError if response.is_a? Net::HTTPServerError
-    response.body
   end
 
   # Get all registered smart home thermostats if no arguments are passed,
   # or create/update a smart home thermostat with the provided values
   def thermostats(values = {})
     return false unless @system_id
+
     uri = "#{API_ENDPOINT}/#{@system_id}/smarthome/thermostats"
-    response = nil
     if values.empty?
-      response = @oauth.get(uri)
+      request(:get, uri).body
     else
       raise ArgumentError, '`values` must contain externalId and name' if values['externalId'].nil? || values['name'].nil?
-      extheader = { 'content-type' => 'application/json' }
-      response = @oauth.post(uri, values.to_json, extheader)
-      return response.message
+
+      request(:post, uri, nil, values.to_json).message
     end
-    raise AuthorizationError, response.body if response.is_a? Net::HTTPUnauthorized
-    raise RateLimitError if response.is_a? Net::HTTPTooManyRequests 
-    raise ServerError if response.is_a? Net::HTTPServerError
-    response.body
+  end
+
+  private
+
+  def request(verb, uri, query = nil, body = nil)
+    extheader = { 'accept' => 'application/json' }
+    res = case verb
+          when :get
+            @oauth.get(uri, query, extheader)
+          when :post, :put
+            extheader['content-type'] = 'application/json'
+            @oauth.send(verb, uri, body, extheader)
+          end
+    raise AuthorizationError, res.body if res.is_a? Net::HTTPUnauthorized
+    raise RateLimitError if res.is_a? Net::HTTPTooManyRequests
+    raise ServerError if res.is_a? Net::HTTPServerError
+
+    res
   end
 end
